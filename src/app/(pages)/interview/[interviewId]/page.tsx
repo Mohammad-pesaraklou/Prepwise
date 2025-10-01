817843de5f76da4641f9d164b6e9bfe72baddc2e
Mhwmd <mhmdpsr6@gmail.com>
2025-10-01 06:27:20 +0330

    create routes and structure

---
+++ src/app/(pages)/interview/[interviewId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InterviewDetails() {
+  return (
+    <div>
+      <h1>InterviewDetails</h1>
+    </div>
+  );
+}
+
+export default InterviewDetails;
---
+++ src/app/(pages)/interview/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InterviewPage() {
+  return (
+    <div>
+      <h1>Interview Page</h1>
+    </div>
+  );
+}
+
+export default InterviewPage;
